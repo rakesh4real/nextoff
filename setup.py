3912d1b05cc43d4657f52da736e948c5437dd559
Asapanna Rakesh <rakeshark22@gmail.com>
2020-09-18 02:06:55 +0530

    plt added

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
   keywords = ['Keras', 'hyperparameters', 'hyperparameters tuning'],   # Keywords that define your package best
   install_requires=[        
           'keras',
+          'matplotlib'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
