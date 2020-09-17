3dde7e2e5bef5cfcc9b6e4348c34ea39515d0d45
Asapanna Rakesh <rakeshark22@gmail.com>
2020-09-18 05:27:13 +0530

    erorr cause commented

--- nextoff/__init__.py
+++ nextoff/__init__.py
@@ -2,6 +2,6 @@
 # =================================
 # specific acessible
 from nextoff.testfile import TestClass
-from nextoff.data.inmemory import InMemoryImgHandler
+# from nextoff.data.inmemory import InMemoryImgHandler
 # won't make all accessible?
 import nextoff
