d57f7dae8069e8bff4966a5618ff4cc4d13deecd
Asapanna Rakesh <rakeshark22@gmail.com>
2020-09-30 17:08:15 +0530

    data commented

--- nextoff/data/inmemory.py
+++ nextoff/data/inmemory.py
@@ -2,24 +2,45 @@ import keras #for one-hot-encoding
 import math 
 
 class InMemoryImgHandler:
+    """ Handles image data available in RAM
+
+    + Splits and stores data in-memory
+    + Creates one-hot-encoded targets if specified
+
+    Attributes
+    ___________
+    - x_train  : np.ndarray
+    - x_test   : np.ndarray
+    - y_train  : np.ndarray
+    - y_test   : np.ndarray
+
+    Methods
+    _______ 
+    - train_val_test_split(X: np.ndarray, y: np.ndarray, ratio: tuple)
+    - one_hot_encode(y: np.ndarray, num_classes: int)
     """
-    Handles image data available in RAM
-    """
+
+    
     def __init__(self, X, y, y_info, ratio=(60,20,20)):
         """
-        + `X` is 4-D ndarray of (n_samples, w, h, c)
-        + `y` is  either one-hot-encoded (n_smaple, n_classes) bin 
-           numeric (n_smaple, 1) ndarray
-        + y_info is tuple
-            - idx 2{INT}  : num_classes
-            - idx 1{BOOL} : True if one-hot-encoded already
-        + `ratio` is tuple (train_ratio, val_ratio, test_ratio)
+        - X: np.ndarray
+            + np.nd array (n_samples, w, h, c)
+        - y: np.ndarray
+            + Either one-hot-encoded (n_smaple, n_classes) bin matrix
+            + or numeric (n_smaple, 1) vec
+        - y_info: tuple
+            - y_info[0]: int 
+                + num_classes
+            - y_info[1]: bool 
+                + True if one-hot-encoded already
+        - ratio: tuple = (60,20,20)
+            + (train_ratio, val_ratio, test_ratio) between 0-100 and sum to 100
         """
-        # 1. one hot encode targets if not already
+
         if y_info[1] is False:
             y = self.one_hot_encode(y, y_info[0])
-                
-        # 2. gen train, val and test dataset and store
+        
+        # split and store                
         (self.x_train, self.y_train), (self.x_val, self.y_val), (self.x_test, self.y_test) = \
             InMemoryImgHandler.train_val_test_split(X, y, ratio)
 
@@ -28,14 +49,25 @@ class InMemoryImgHandler:
     # ============================================================================================
     @staticmethod
     def train_val_test_split(X, y, ratio):
-        """ 
-        + `X` is 4-D array of (n_samples, w, h, c) 
-        + `y` is one-hot-ecoded
-        + `ratio` is tuple (train_ratio, val_ratio, test_ratio)
-        + returns tuple (X_tr, y_tr), (X_val, y_val), (X_te, y_te)
+        """ Splits data into train test and validation
 
-        Note: not shufflind cz, it will be taken care by fit function
+        Parameters
+        __________
+        - X: np.ndarray 
+            + 4-D (n_samples, w, h, c) 
+        - y: np.ndarray
+            + one-hot-encoded bin matrix
+        - ratio: tuple 
+            + (train_ratio, val_ratio, test_ratio)
+
+        Returns 
+        _______
+        tuple 
+            + (X_tr, y_tr), (X_val, y_val), (X_te, y_te)
+
+        * Note: Not shufflind cz, taken care by `model.fit`
         """
+
         if (ratio[0] + ratio[1] + ratio[2]) != 100:
             raise Exception('Split percents\' sum must be 100!')
 
